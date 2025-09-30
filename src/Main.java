20b92318f52035bcacbeda022467b542e544b464
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-09-30 15:16:21 +0100

    add connectio with JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import repository.DatabaseConnection;
+import resources.ConfigDB;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("hiiii");
+        try {
+            ConfigDB configDB=new ConfigDB();
+            DatabaseConnection db =  DatabaseConnection.getInstance(configDB);
+            Connection conn=db.getConnection();
+
+            if (conn != null) {
+                System.out.println("Connecté !!!");
+            } else {
+                System.out.println("Échec de connexion !!!");
+            }
+
+        } catch ( SQLException | ClassNotFoundException e) {
+
+            System.out.println("erooooor");
+            e.printStackTrace();
+        }
     }
 }
--- src/repository/DatabaseConnection.java
+++ src/repository/DatabaseConnection.java
@@ -1,5 +1,38 @@
 package repository;
 
+import resources.ConfigDB;
+
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+
 public class DatabaseConnection {
+
+    private static DatabaseConnection instence;
+    private Connection connection;
+    private ConfigDB configDB ;
+
+    private DatabaseConnection(ConfigDB configDB) throws ClassNotFoundException, SQLException {
+        this.configDB= new ConfigDB();
+        Class.forName(configDB.getDriver());
+        this.connection = DriverManager.getConnection(
+                configDB.getURL(),
+                configDB.getRoot(),
+                configDB.getPassword()
+        );
+    }
+
+    public static DatabaseConnection getInstance(ConfigDB configDB) throws SQLException , ClassNotFoundException{
+        if(instence == null){
+            instence= new DatabaseConnection(configDB);
+        }
+        return instence;
+    }
+
+    public Connection getConnection (){
+        return connection;
+    }
 
 }
