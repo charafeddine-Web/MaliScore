3bf81f780c53b1e1fa0bd34897fee21dafa2b217
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-10-01 19:15:20 +0100

    add CreditService

--- src/service/CreditService.java
+++ src/service/CreditService.java
@@ -1,4 +1,35 @@
 package service;
 
+import model.Credit;
+import repository.CreditRepository;
+
+import java.util.List;
+
 public class CreditService {
+    private CreditRepository creditRepository;
+
+    public CreditService(){
+        this.creditRepository = new CreditRepository();
+    }
+    public void addCredit(Credit c){
+        creditRepository.save(c);
+    }
+
+    public void updateCredit(Credit c){
+        creditRepository.update(c);
+    }
+
+    public void deleteCredit(long id){
+        creditRepository.delete(id);
+    }
+
+    public List<Credit> findAllCredit(){
+        return creditRepository.findAll();
+    }
+
+    public Credit findCreditById(long id){
+        return creditRepository.findById(id);
+    }
+
+
 }
